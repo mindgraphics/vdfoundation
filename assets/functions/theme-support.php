8697b8c728c330a4e57af82c63ae265aad596be0
mindgraphics <michaelbenson@mindgraphics.co.uk>
2017-05-20 16:43:36 +0100

    Add files via upload

---
+++ assets/functions/theme-support.php
@@ -0,0 +1,61 @@
+<?php
+	
+// Adding WP Functions & Theme Support
+function joints_theme_support() {
+
+	// Add WP Thumbnail Support
+	add_theme_support( 'post-thumbnails' );
+	
+	// Default thumbnail size	
+	set_post_thumbnail_size( 1000, 600, true );
+	add_image_size( 'vdproduction-post-thumb', 1000, 600, true );
+	add_image_size( 'vdproduction-mid-thumb', 400, 240, true );
+	add_image_size( 'vdproduction-small-thumb', 100, 100, true );
+
+	//Custom Header support
+	$defaults = array(
+		'default-image' => '',
+		'flex-width'    => true,
+		'width'         => 1400,
+		'flex-height'   => true,
+		'height'        => 600,
+	);
+	add_theme_support( 'custom-header', $defaults );
+
+	
+	// Add RSS Support
+	add_theme_support( 'automatic-feed-links' );
+	
+	// Add Support for WP Controlled Title Tag
+	add_theme_support( 'title-tag' );
+	
+	// Add HTML5 Support
+	add_theme_support( 'html5', 
+	         array( 
+	         	'comment-list', 
+	         	'comment-form', 
+	         	'search-form', 
+	         ) 
+	);
+	
+	// Adding post format support
+	 add_theme_support( 'post-formats',
+		array(
+			'aside',             // title less blurb
+			'gallery',           // gallery of images
+			'link',              // quick link to other site
+			'image',             // an image
+			'quote',             // a quick quote
+			'status',            // a Facebook like status update
+			'video',             // video
+			'audio',             // audio
+			'chat'               // chat transcript
+		)
+	); 
+	
+	// Set the maximum allowed width for any content in the theme, like oEmbeds and images added to posts.
+	$GLOBALS['content_width'] = apply_filters( 'joints_theme_support', 1200 );	
+	
+} /* end theme support */
+
+add_action( 'after_setup_theme', 'joints_theme_support' );
